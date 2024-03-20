f96f5de643594b21ddd67836be8a041cf8351760
KickDown <97972746+Kickz1nn@users.noreply.github.com>
2024-03-20 10:43:08 -0300

    Update index.js

--- DOM/js/index.js
+++ DOM/js/index.js
@@ -1,7 +1,12 @@
-document.getElementById("paragrafo").innerHTML("Olá"); //Pega o elemento pelo ID
+function dom1(){
+    document.getElementById("paragrafo").innerHTML = "Olá"; //Pega o elemento pelo ID
+    document.getElementsByClassName("p2").innerHTML = "Hello World!"; //Pega o elemento pela Classe
+    a = document.getElementsByTagName("p"); // Pega o elemento pela tag
+    document.querySelector(".p2").style.color = "red"; // Pega o primeiro elemento css com o nome passado
+    a[2].innerHTML = "Oi rsrs"
+}
 
-document.getElementsByClassName("p2").innerHTML("Bom Dia"); //Pega o elemento pela Classe
- 
-document.getElementsByTagName("label").innerHTML("Olá, bom dia") // Pega o elemento pela tag
-
-document.querySelector(".p2").style.color
+function dom2() {
+    b = document.querySelectorAll(".a"); // Pega todos os elementos css com o nome passado
+    b[0, 1].style.color = "blue";
+}
