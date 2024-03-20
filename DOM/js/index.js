1bb23dd154125c4fc078b3d32aff411e31fd7cc9
KickDown <97972746+Kickz1nn@users.noreply.github.com>
2024-03-20 09:45:47 -0300

    Create index.js

---
+++ DOM/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("paragrafo").innerHTML("Olá"); //Pega o elemento pelo ID
+
+document.getElementsByClassName("p2").innerHTML("Bom Dia"); //Pega o elemento pela Classe
+ 
+document.getElementsByTagName("label").innerHTML("Olá, bom dia") // Pega o elemento pela tag
+
+document.querySelector(".p2").style.color
